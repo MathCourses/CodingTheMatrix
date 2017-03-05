390ab25abf0cc47631828356a91bbe3d2d70b331
An Doan <minh.an.doan@gmail.com>
2017-03-05 14:24:05 -0800

    Create a procedure that solves a triangular system of linear euqaltions.

--- vecutil.py
+++ vecutil.py
@@ -6,6 +6,31 @@ def zero_vec(D):
 def list2vec(L):
     return Vec(set(range(len(L))), {k:x for k,x in enumerate(L)})
 
+def triangular_solve(rowlist, b):
+    """
+    Solve Ax = b for vector x and rowlist = A
+    :param rowlist: a representation of matrix A
+    :param b: representation for solution vector b
+    :return: representation of vector x
+    """
+    x = zero_vec(rowlist[0].D)
+    for i in reversed(range(len(rowlist))):
+        x[i] = (b[i] - rowlist[i] * x)/rowlist[i][i]
+    return x
+
+def triangular_solve(rowlist,label_list, b):
+    """
+    Solve Ax = b for vector x and rowlist = A
+    For arbitrary domains, need to specify an order for which the system is "triangular"
+    :param rowlist: a representation of matrix A
+    :param b: representation for solution vector b
+    :return: representation of vector x
+    """
+    x = zero_vec(set(label_list))
+    for r in reversed(range(len(rowlist))):
+        c = label_list[r]
+        x[c] = (b[r] - rowlist[r] * x)/rowlist[r][c]
+    return x
 
 list = ['A', 'B', 'C', 'D']
 v = list2vec(list)
